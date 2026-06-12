910d9375a1095cba66b81c3e41f17de4e0e321be
Johansel12 <johansel1207@gmail.com>
2026-06-12 19:22:03 -0400

    crear controladores usando DTOs
    Se crean los controladores Cliente, Mesa, Turno y Reserva utilizando DTOs y almacenamiento en memoria.

---
+++ RestauranReservation.API/Controllers/ClienteControler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using RestauranReservation.API.DTOs;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClienteController : ControllerBase
+{
+    private static List<ClienteDTO> clientes = new List<ClienteDTO>();
+
+    [HttpGet]
+    public IEnumerable<ClienteDTO> Get()
+    {
+        return clientes;
+    }
+
+    [HttpPost]
+    public IActionResult Post(ClienteDTO cliente)
+    {
+        clientes.Add(cliente);
+        return Ok(cliente);
+    }
+}
