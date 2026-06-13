4f24bb868ef19e69bdfad2ca68729cb81a2f9b05
Johansel12 <johansel1207@gmail.com>
2026-06-12 22:48:00 -0400

    Update Program.cs

--- RestaurantReservation.API/Program.cs
+++ RestaurantReservation.API/Program.cs
@@ -2,8 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer(); // ?
-builder.Services.AddSwaggerGen(); // ?
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen(); 
 
 var app = builder.Build();
 
