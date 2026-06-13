3ce3405cf07fb2ffaa40ab1e7fbaf025141630d9
Johansel12 <johansel1207@gmail.com>
2026-06-13 03:18:31 -0400

    implementar CRUD completo con Entity Framework y SQL Server
    Se implementa CRUD completo en todas las entidades (GET, POST, PUT y DELETE).

--- RestaurantReservation.API/Controllers/TurnoController.cs
+++ RestaurantReservation.API/Controllers/TurnoController.cs
@@ -14,17 +14,47 @@ public class TurnoController : ControllerBase
         _context = context;
     }
 
+    //GET
     [HttpGet]
     public async Task<IEnumerable<Turno>> Get()
     {
         return await _context.Turnos.ToListAsync();
     }
 
+    //POST
     [HttpPost]
     public async Task<IActionResult> Post(Turno turno)
     {
         _context.Turnos.Add(turno);
         await _context.SaveChangesAsync();
         return Ok(turno);
+    }
+
+    //PUT
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Turno turno)
+    {
+        if (id != turno.Id)
+            return BadRequest();
+
+        _context.Entry(turno).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return Ok(turno);
+    }
+
+    //DELETE
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var turno = await _context.Turnos.FindAsync(id);
+
+        if (turno == null)
+            return NotFound();
+
+        _context.Turnos.Remove(turno);
+        await _context.SaveChangesAsync();
+
+        return Ok();
     }
 }
