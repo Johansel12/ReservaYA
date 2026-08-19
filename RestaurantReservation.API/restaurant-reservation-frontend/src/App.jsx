83ff075143febe7b5af2edf0248fb937243b4435
Johansel12 <johansel1207@gmail.com>
2026-08-19 04:11:11 -0400

    configurar navegacion principal

--- RestaurantReservation.API/restaurant-reservation-frontend/src/App.jsx
+++ RestaurantReservation.API/restaurant-reservation-frontend/src/App.jsx
@@ -1,7 +1,47 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import Clientes from "./pages/Clientes";
+import Mesas from "./pages/Mesas";
+import Turnos from "./pages/Turnos";
+import Reservas from "./pages/Reservas";
 
 function App() {
-    return <Clientes />;
+    return (
+        <BrowserRouter>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">
+                        Restaurant Reservation
+                    </Link>
+
+                    <div className="navbar-nav">
+                        <Link className="nav-link" to="/">
+                            Clientes
+                        </Link>
+
+                        <Link className="nav-link" to="/mesas">
+                            Mesas
+                        </Link>
+
+                        <Link className="nav-link" to="/turnos">
+                            Turnos
+                        </Link>
+
+                        <Link className="nav-link" to="/reservas">
+                            Reservas
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Clientes />} />
+                <Route path="/mesas" element={<Mesas />} />
+                <Route path="/turnos" element={<Turnos />} />
+                <Route path="/reservas" element={<Reservas />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ RestaurantReservation.API/restaurant-reservation-frontend/src/pages/Reservas.jsx
@@ -0,0 +1,5 @@
+function Reservas() {
+    return <h1>Reservas</h1>;
+}
+
+export default Reservas;
